67a911493d1b52bfe5a5f028376260c1f0efcd99
AmirHossein Parhizgar <ahparhizgar@gmail.com>
2025-03-19 01:35:45 +0330

    Implement componentCoroutineScope

--- shared/src/commonMain/kotlin/io/amirhparhizgar/kotask/list/TodoListComponent.kt
+++ shared/src/commonMain/kotlin/io/amirhparhizgar/kotask/list/TodoListComponent.kt
@@ -3,13 +3,14 @@ package io.amirhparhizgar.kotask.list
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
 import com.arkivanov.decompose.value.Value
-import com.arkivanov.essenty.lifecycle.doOnResume
+import com.arkivanov.essenty.lifecycle.doOnDestroy
 import com.arkivanov.essenty.lifecycle.doOnStart
 import com.arkivanov.essenty.lifecycle.doOnStop
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 interface TodoListComponent {
@@ -21,13 +22,14 @@ class DefaultTodoListComponent(
     private val repo: FakeTodoRepository
 ) : TodoListComponent,
     ComponentContext by componentContext {
+    private val coroutineScope = componentCoroutineScope()
     private val _items: MutableValue<List<Todo>> = MutableValue(emptyList())
     override val items: Value<List<Todo>> = _items
 
     init {
         var job: Job? = null
         lifecycle.doOnStart {
-            job = CoroutineScope(Dispatchers.Main).launch {
+            job = coroutineScope.launch {
                 repo.todos.collect {
                     _items.value = it
                 }
@@ -35,4 +37,12 @@ class DefaultTodoListComponent(
         }
         lifecycle.doOnStop { job?.cancel() }
     }
+}
+
+fun ComponentContext.componentCoroutineScope(): CoroutineScope {
+    val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+    lifecycle.doOnDestroy {
+        scope.cancel()
+    }
+    return scope
 }

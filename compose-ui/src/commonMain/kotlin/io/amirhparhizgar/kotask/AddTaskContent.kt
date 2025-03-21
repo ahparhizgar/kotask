c3b177a98aad278da0a8616a42e8c5a4b906e8a5
AmirHossein Parhizgar <ahparhizgar@gmail.com>
2025-03-22 01:23:56 +0330

    Implement AddTaskContent

---
+++ compose-ui/src/commonMain/kotlin/io/amirhparhizgar/kotask/AddTaskContent.kt
@@ -0,0 +1,31 @@
+package io.amirhparhizgar.kotask
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.arkivanov.decompose.extensions.compose.subscribeAsState
+
+@Composable
+fun AddTaskContent(
+    component: AddTaskComponent,
+    modifier: Modifier,
+) {
+    val state by component.state.subscribeAsState()
+    Row(modifier = modifier) {
+        TextField(
+            value = state.title,
+            onValueChange = component::onTitleChange,
+        )
+        Spacer(Modifier.weight(1f))
+        Button(
+            onClick = component::onAddClick,
+        ) {
+            Text("Add")
+        }
+    }
+}
